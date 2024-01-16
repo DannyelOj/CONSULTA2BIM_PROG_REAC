82197182960906ffded20f3453a6705c2a3b951f
DannyelOj <115769565+DannyelOj@users.noreply.github.com>
2024-01-15 22:10:58 -0500

    Add files via upload
    Manifiesto_Reactivo_Consulta

---
+++ ManifiestoReactivo/src/manifiestoreactivo/ManifiestoReactivo.java
@@ -0,0 +1,48 @@
+/*
+¿Qué son los sistemas reactivos?
+
+Los sistemas reactivos son una poderosa herramienta para crear aplicaciones de 
+software modernas, escalables y resistentes que puedan satisfacer las necesidades
+de los entornos complejos y dinámicos de hoy en día.
+
+Los sistemas reactivos, tal como se describen en el Manifiesto Reactivoson
+sistemas que se diseñan utilizando principios reactivos para ser más resistentes,
+receptivos, elásticos y basados en mensajes. En general, esto significa que los 
+sistemas reactivos son más fáciles de desarrollar y escalar, tienen mayor 
+capacidad de respuesta y son significativamente más tolerantes a fallos que los 
+sistemas que utilizan una arquitectura monolítica más tradicional.
+
+son más adecuados para un mundo en el que se espera y se exige que las aplicaciones 
+desplegadas en la nube o en dispositivos móviles tengan un tiempo de respuesta de 
+milisegundos y un tiempo de actividad de casi el 100%. Para satisfacer estas demandas,
+los sistemas reactivos se componen de módulos débilmente acoplados llamados 
+microservicios (piense en un programa dentro de un programa) que utilizan los 
+principios reactivos antes mencionados.
+
+La programación reactiva es un paradigma declarativo que permite responder 
+automáticamente a cambios en los datos, mejorando la eficiencia, robustez y
+mantenibilidad de las aplicaciones.
+
+Se entiende por escalabilidad a la capacidad de adaptación y respuesta de un 
+sistema con respecto al rendimiento del mismo a medida que aumentan de forma 
+significativa el número de usuarios del mismo.
+
+Reactivo es algo que responde con “facilidad a un estímulo”. Los sistemas reactivos 
+reaccionan a los eventos (dirigidos por eventos), reaccionan a la carga (escalables), 
+reaccionan a los fallos y errores (son resilientes) y reaccionan a los usuarios 
+(son responsivos).
+
+
+Dirigidas por eventos
+Aplicaciones que reaccionan a los eventos son aplicaciones que ofrecen flexibilidad 
+y ayudan a reducir los costes de mantenimiento, pues su diseño se basa en una 
+comunicación asíncrona y donde el interfaz tiene la posibilidad de centrarse en 
+el contenido de la comunicación. El destinatario y el remitente son implementados 
+si tener en cuenta los detalles de la propagación del evento. El destinatario se
+activa en el momento en que un mensaje es recibido, o ocurre un evento. Esto permite 
+un uso eficiente de los recursos y permite que un número importante de destinatarios 
+puedan compartir un único subproceso de hardware. Sistemas dirigidos por eventos 
+tienden a basarse en el push, en lugar del pull.
+
+
+*/
